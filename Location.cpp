b0f4afab6cc3030b256b75e59ac78dd9cd907589
Ron Bondy <ad0dx@yahoo.com>
2016-12-09 07:58:28 -0600

    Add files via upload

---
+++ Location.cpp
@@ -0,0 +1,34 @@
+
+#include "stdafx.h"
+#include "Location.h"
+#include "Qso.h"
+
+Location::Location(Qso *qso)
+   :
+   QsoItem(qso)
+   {
+   }
+
+Location::~Location()
+{
+}
+
+// Return true if location is valid
+// Return false if location is invalid and set error string
+bool Location::Validate(const set<string>& validLocations)
+{
+   if (m_value.empty())
+   {
+      m_errorString = "Location is missing";
+      return false;
+   }
+
+   auto iter = validLocations.find(m_value);
+   if (iter == validLocations.end())
+   {
+      m_errorString = m_value + string(" is not a valid location");
+      return false;
+   }
+
+   return true;
+}
